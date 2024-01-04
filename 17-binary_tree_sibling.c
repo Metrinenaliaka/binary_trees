431b02839cf36fc3da4a4daf441c5204ad69b881
Metrinenaliaka <metrinenam@gmail.com>
2024-01-04 04:37:25 -0800

    checking for sibling

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+/**
+* binary_tree_sibling - checks for sibling of node
+* @node: node on tree to check
+* Return: sibling or NULL
+*/
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+
+	if (node == NULL)
+		return (NULL);
+
+	if (node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->right == NULL || node->parent->left == NULL)
+		return (NULL);
+
+	if (node->parent->right != NULL && node->parent->right != node)
+		return (node->parent->right);
+	if (node->parent->left != NULL && node->parent->left != node)
+		return (node->parent->left);
+	return (NULL);
+}
